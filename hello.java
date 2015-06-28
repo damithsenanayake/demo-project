b2d5a2c61cd7cdefa2a554a400bbcb84b8de6437
Dscript <dscriptcreations@gmail.com>
2015-06-28 11:05:46 +0530

    dan hari

--- hello.java
+++ hello.java
@@ -4,6 +4,7 @@
 
 public class hello{
 	public static void main(String[] args){
-		System.out.println("Hello world!");
+		System.out.println("hello world");
+		System.out.println("thota hena gahapan");
 	}
 }
